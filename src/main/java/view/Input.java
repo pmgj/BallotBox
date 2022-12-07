a318be202badf4faab6c11b1ebdf9244a77f9f6e
Paulo Gonçalves <paulogoncalves@recife.ifpe.edu.br>
2022-12-07 15:55:59 -0300

    Improved GUI

--- src/main/java/view/Input.java
+++ src/main/java/view/Input.java
@@ -14,25 +14,45 @@ public class Input extends JPanel {
     private int officeIndex = 0;
     private int numberIndex = 0;
     private JLabel lblOffice;
-    private JLabel lblTop = new JLabel("SEU VOTO PARA");
-    private JLabel lblNumber = new JLabel("Número:");
+    private JLabel lblTop = new JLabel();
+    private JLabel lblNumber = new JLabel();
     private JLabel[] lblDigits;
+    private JPanel jpDigits = new JPanel();
+    private JLabel lblFoot = new JLabel();
+    private JLabel lblFoot1 = new JLabel();
+    private JLabel lblFoot2 = new JLabel();
+    private JLabel lblFoot3 = new JLabel();
 
     public Input(Cargo[] cargos) {
         this.offices = cargos;
         var cargo = this.offices[this.officeIndex];
         this.setLayout(null);
-        this.lblTop.setBounds(0, 0, 300, 20);
+        this.lblTop.setBounds(3, 0, 300, 20);
         this.lblTop.setFont(new Font("SansSerif", Font.PLAIN, 12));
         this.add(this.lblTop);
         this.lblOffice = new JLabel(cargo.getNome());
         this.lblOffice.setBounds(70, 50, 300, 30);
         this.lblOffice.setFont(new Font("SansSerif", Font.BOLD, 24));
         this.add(this.lblOffice);
-        this.lblNumber.setBounds(0, 150, 150, 20);
+        this.lblNumber.setBounds(3, 150, 150, 20);
         this.lblNumber.setFont(new Font("SansSerif", Font.PLAIN, 12));
         this.add(this.lblNumber);
         this.createLabels(cargo.getQuantidadeNumeros());
+        this.jpDigits.setBounds(70, 140, 300, 45);
+        this.add(this.jpDigits);
+        int initialHeight = 300, stepHeight = 20;
+        this.lblFoot.setBounds(0, initialHeight, 550, 20);
+        this.lblFoot.setFont(new Font("SansSerif", Font.PLAIN, 12));
+        this.add(this.lblFoot);
+        this.lblFoot1.setBounds(3, initialHeight + stepHeight, 200, 20);
+        this.lblFoot1.setFont(new Font("SansSerif", Font.PLAIN, 12));
+        this.add(this.lblFoot1);
+        this.lblFoot2.setBounds(36, initialHeight + 2 * stepHeight, 200, 20);
+        this.lblFoot2.setFont(new Font("SansSerif", Font.PLAIN, 12));
+        this.add(this.lblFoot2);
+        this.lblFoot3.setBounds(45, initialHeight + 3 * stepHeight, 250, 20);
+        this.lblFoot3.setFont(new Font("SansSerif", Font.PLAIN, 12));
+        this.add(this.lblFoot3);
     }
 
     private void createLabels(int quantity) {
@@ -42,14 +62,15 @@ public class Input extends JPanel {
             this.lblDigits[i].setBorder(BorderFactory.createLineBorder(Color.black));
             this.lblDigits[i].setFont(new Font("SansSerif", Font.PLAIN, 36));
             this.lblDigits[i].setPreferredSize(new Dimension(40, 40));
-            this.lblDigits[i].setBounds(70 + 42 * i, 140, 40, 40);
-            this.add(this.lblDigits[i]);
+            this.jpDigits.add(this.lblDigits[i]);
         }
     }
 
     public void confirma() {
         this.officeIndex++;
         this.numberIndex = 0;
+        this.jpDigits.removeAll();
+        this.jpDigits.repaint();
         if (this.officeIndex < this.offices.length) {
             var cargo = this.offices[this.officeIndex];
             this.lblOffice.setText(cargo.getNome());
@@ -57,6 +78,7 @@ public class Input extends JPanel {
         } else {
             this.lblOffice.setText("FIM");
         }
+        this.clearLabels();
     }
 
     public void corrige() {
@@ -64,12 +86,31 @@ public class Input extends JPanel {
         for (var label : this.lblDigits) {
             label.setText("");
         }
+        this.clearLabels();
     }
 
     public void setNumber(String n) {
+        if (this.numberIndex >= 1) {
+            this.lblTop.setText("SEU VOTO PARA");
+            this.lblNumber.setText("Número:");
+            this.lblFoot.setText(
+                    "___________________________________________________________________________________________");
+            this.lblFoot1.setText("Aperte a tecla:");
+            this.lblFoot2.setText("CONFIRMA para PROSSEGUIR");
+            this.lblFoot3.setText("CORRIGE para REINICIAR este VOTO");
+        }
         if (this.numberIndex < this.lblDigits.length) {
             var label = this.lblDigits[this.numberIndex++];
             label.setText(n);
         }
+    }
+
+    private void clearLabels() {
+        this.lblTop.setText("");
+        this.lblNumber.setText("");
+        this.lblFoot.setText("");
+        this.lblFoot1.setText("");
+        this.lblFoot2.setText("");
+        this.lblFoot3.setText("");
     }
 }
